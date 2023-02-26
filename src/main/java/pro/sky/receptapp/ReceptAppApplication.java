1b98fb3e7870a0c4a7f7c7bb4ecc30db10afa6c7
ShevoFirst <play.job@yandex.ru>
2023-02-27 02:39:32 +0400

    second commit

---
+++ src/main/java/pro/sky/receptapp/ReceptAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.receptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/receptapp/ReceptAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.receptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
