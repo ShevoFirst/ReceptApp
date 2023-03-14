4b652220154396f7130bc4730753b036e0b05c42
ShevoFirst <play.job@yandex.ru>
2023-03-14 16:28:41 +0400

    +описание к эндпоинтам и контроллерам

--- src/main/java/pro/sky/receptapp/controller/IngridientController.java
+++ src/main/java/pro/sky/receptapp/controller/IngridientController.java
@@ -1,5 +1,7 @@
 package pro.sky.receptapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.receptapp.model.Ingridient;
@@ -8,16 +10,23 @@ import services.IngridientService;
 
 @RestController
 @RequestMapping("/ingridient")
+@Tag(name = "Ингридиенты", description = "CRUD операции и другие эндпоинты с транзакциями")
 public class IngridientController {
     private IngridientService ingridientServise;
 
     @PostMapping
+    @Operation(
+            summary = "создание ингридиента"
+    )
     public ResponseEntity createIngridient(@PathVariable Ingridient ingridient){
         ingridientServise.addIngridient(ingridient);
         return ResponseEntity.ok(ingridient);
     }
 
     @GetMapping("{id}")
+    @Operation(
+            summary = "получение ингридиента"
+    )
     public ResponseEntity getIngridient(@PathVariable long id){
         Ingridient ingridient = ingridientServise.getIngridient(id);
         if (ingridient == null){
@@ -28,12 +37,18 @@ public class IngridientController {
 
 
     @PutMapping("{id}")
+    @Operation(
+            summary = "редактирование ингридиента"
+    )
     public ResponseEntity editIngridient(@PathVariable long id,@RequestBody Ingridient ingridient){
         ingridientServise.editIngridient(id,ingridient);
         return ResponseEntity.ok(ingridient);
     }
 
     @DeleteMapping("{id}")
+    @Operation(
+            summary = "удаление ингридиента"
+    )
     public ResponseEntity deleteIngridient(@PathVariable long id){
         ingridientServise.deleteIngridient(id);
         return ResponseEntity.ok(id);
--- src/main/java/pro/sky/receptapp/controller/ReceptController.java
+++ src/main/java/pro/sky/receptapp/controller/ReceptController.java
@@ -1,5 +1,7 @@
 package pro.sky.receptapp.controller; 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.receptapp.model.Recept;
@@ -7,17 +9,25 @@ import services.ReceptService;
 
 @RestController
 @RequestMapping("/recept")
+@Tag(name = "Рецепты", description = "CRUD операции и другие эндпоинты с рецептами")
+
 public class ReceptController {
     private ReceptService receptService;
 
 
     @PostMapping
+    @Operation(
+            summary = "создание рецепта"
+    )
     public ResponseEntity createRecept(@RequestBody Recept recept) {
         receptService.addRecept(recept);
         return ResponseEntity.ok(recept);
     }
 
     @GetMapping("{id}")
+    @Operation(
+            summary = "получение рецепта"
+    )
     public ResponseEntity getRecept(@PathVariable long id){
         Recept recept = receptService.getRecept(id);
         if (recept == null){
@@ -27,12 +37,18 @@ public class ReceptController {
     }
 
     @PutMapping("{id}")
+    @Operation(
+            summary = "редактирование рецепта"
+    )
     public ResponseEntity editRecept(@PathVariable long id,@RequestBody Recept recept){
         receptService.editRecept(id,recept);
         return ResponseEntity.ok(recept);
     }
 
     @DeleteMapping("{id}")
+    @Operation(
+            summary = "удаление рецепта"
+    )
     public ResponseEntity deleteRecept(@PathVariable long id){
         receptService.deleteRecept(id);
         return ResponseEntity.ok(id);
