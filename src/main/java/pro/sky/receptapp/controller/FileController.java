176f128c526042a795b7c90403af65b296427cd2
ShevoFirst <play.job@yandex.ru>
2023-03-26 23:39:11 +0400

    Updte

--- src/main/java/pro/sky/receptapp/controller/FileController.java
+++ src/main/java/pro/sky/receptapp/controller/FileController.java
@@ -3,6 +3,7 @@ package pro.sky.receptapp.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.io.IOUtils;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,18 +11,23 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import pro.sky.receptapp.model.Recept;
 import pro.sky.receptapp.services.FilesService;
+import pro.sky.receptapp.services.ReceptService;
 
 import java.io.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/files")
 @Tag(name = "Работа с файлами", description = "CRUD операции и другие эндпоинты с файлами")
 public class FileController {
     private final FilesService filesService;
+    private final ReceptService recipeService;
 
-    public FileController(FilesService filesService) {
+    public FileController(FilesService filesService, ReceptService recipeService) {
         this.filesService = filesService;
+        this.recipeService = recipeService;
     }
     @GetMapping("/get/{fileName}")
     @Operation(
@@ -53,4 +59,18 @@ public class FileController {
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+    @GetMapping("/get/recept")
+    @Operation(
+            summary = "получение TXT файла рецептов"
+    )
+    public ResponseEntity<InputStreamResource> downloadReceptTXTFile() throws FileNotFoundException {
+        File file = filesService.getDataFile("recept.json");
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(file.length())
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"Recipes.txt\"")
+                .body(resource);
+    }
+
 }
